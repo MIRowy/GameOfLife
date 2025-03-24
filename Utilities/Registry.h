1ac9f35a72ba97c74283547afdf354c0dc6e5cc1
jc <juliusz.czechura@gmail.com>
2025-03-24 11:38:46 +0100

    Added Registry implementation and bumped minor

---
+++ Utilities/Registry.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <map>
+
+using namespace std;
+
+template<class TKey, class TValue>
+class Registry
+{
+    map<TKey, TValue> _internalMap;
+
+public:
+    void Register(const TKey key, const TValue value)
+    {
+        if (_internalMap.contains(key))
+        {
+            throw std::invalid_argument("Key already registered in the registry.");
+        }
+
+        _internalMap[key] = value;
+    }
+
+    [[nodiscard]]
+    TValue Get(const TKey key) const
+    {
+        auto it = _internalMap.find(key);
+
+        if (it != _internalMap.end())
+        {
+            return it->second;
+        }
+
+        throw std::out_of_range("Key not found in registry.");
+    }
+};
