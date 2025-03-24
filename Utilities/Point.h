1ec0529aa75467d204e2400613e9f78d0c2cc04c
jc <juliusz.czechura@gmail.com>
2025-03-24 11:04:24 +0100

    Added Point.h utility class

---
+++ Utilities/Point.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Point
+{
+  const int _x;
+  const int _y;
+
+public:
+  constexpr Point(const int x, const int y) : _x(x), _y(y) {}
+
+  [[nodiscard]] constexpr int X() const { return _x; }
+  [[nodiscard]] constexpr int Y() const { return _y; }
+};
